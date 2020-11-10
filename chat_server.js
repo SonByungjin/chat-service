115e6a587dd46147bfbcc85df1b260532ebb6402
sonbyungjin <sgyos000@gmail.com>
2020-11-10 19:25:08 +0900

    second version

--- chat_server.js
+++ chat_server.js
@@ -14,14 +14,19 @@ server.listen(port, () => {
 
 app.get('/', function (req, res, next) {
   res.json('hi');
-  // server.listen(3000, () => {
-  //   console.log('listenToServer');
-  // });
 });
 
 io.on('connection', (socket) => {
+  socket.on('newUser', (data) => {
+    io.emit('enter', data);
+  });
+
   socket.on('message', (data) => {
     console.log('client가 보낸 데이터: ', data);
     io.emit('upload', data);
+  });
+
+  socket.on('leaveUser', (nick) => {
+    io.emit('out', nick);
   });
 });
